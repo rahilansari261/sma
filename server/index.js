aa2dd9a894ba849cf3a96bb4c6c487f3ea5a5e82
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2022-12-22 13:56:02 +0000

    app up and running

--- server/index.js
+++ server/index.js
@@ -27,15 +27,17 @@ app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
 app.use(morgan('common'))
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
 app.use(cors())
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/assets')
+    cb(null, 'uploads/assets')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
@@ -44,6 +46,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
+
 app.post('/auth/register', upload.single('picture'), register)
 app.post('/posts', verifyToken, upload.single('picture'), createPost)
 
